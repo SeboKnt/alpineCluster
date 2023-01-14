05d714347766471d4cdad48c7db18b637c780302
Sebastian <sebastian.alxr@gmail.com>
2023-01-15 00:26:09 +0100

    init

---
+++ priming.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ssh root@schwalbe.youngandhungry.org "apk add python3 curl"
+ssh root@spatz.youngandhungry.org "apk add python3 curl"
+ssh root@zwerggans.youngandhungry.org "apk add python3 curl"
