bc0b89024ccabeadf0ed0112692ff86d97063e19
Sebastian <105110061+JosebStahl@users.noreply.github.com>
2023-01-15 10:50:23 +0000

    fix hosts

--- priming.sh
+++ priming.sh
@@ -6,6 +6,6 @@ server=("schwalbe" "spatz" "zwerggans")
 
 for hosts in "${server[@]}"
 do
-   ssh-copy-id -i ~/.ssh/*.pub root@$hosts
+   ssh-copy-id -i ~/.ssh/*.pub root@$hosts.youngandhungry.org
    ssh root@$hosts.youngandhungry.org "apk add python3"
 done
