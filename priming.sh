82ec1a7f12e600e1b76b1e2cb4cbd66a5df20a3f
Sebastian <105110061+JosebStahl@users.noreply.github.com>
2023-01-15 11:19:17 +0000

    halftime

--- priming.sh
+++ priming.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt update -y
+sudo apt install ansible -y
+
 ssh-keygen -t rsa -b 4096
 
 server=("schwalbe" "spatz" "zwerggans")
-
 for hosts in "${server[@]}"
 do
    ssh-copy-id -i ~/.ssh/*.pub root@$hosts.youngandhungry.org
