47657a18ba3b8cb7a675431e93dcc89086360a56
Sebastian <105110061+JosebStahl@users.noreply.github.com>
2023-02-01 10:43:55 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ ansible-playbook install.yaml -i inventory/hosts.yaml
 
 ## FOR more DEBUG info
 #ansible-playbook -vvv install.yaml -i inventory/hosts.yaml
+
+## For Example Ansible secret
+#ansible-playbook maintenance.yaml -i inventory/hosts.yaml --ask-vault-pass
