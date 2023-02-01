4d04ffe272e92c0072fae9efbc1b7b3bc5bf7cc1
Sebastian <105110061+JosebStahl@users.noreply.github.com>
2023-02-01 10:40:15 +0000

    automate secret creation

--- createSecrets.sh
+++ createSecrets.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# USAGE: bash createSecrets.sh <variable> <password>
+USAGE="bash createSecrets.sh <variable> <password>"
+path="./inventory/group_vars/all.yaml"
 
-echo $1 >> ./inventory/group_vars/all.yaml
+if [ -z "$2" ]
+then
+  echo "PLEASE USE THE SYNTAX: $USAGE"
+  exit
+fi
 
-ansible-vault encrypt_string $2 --ask-vault-pass >> ./inventory/group_vars/all.yaml
+secret=$(ansible-vault encrypt_string $2 --ask-vault-pass)
+echo "$1: $secret" >> $path
+echo "folgendes wurde in $path gespeichert"
+tail -n 7 $path
 
 # HIER FEHLT NOCH GANZE DATEIEN ZU VERSCHLÜSSELN
 # https://docs.ansible.com/ansible/latest/vault_guide/vault_encrypting_content.html
