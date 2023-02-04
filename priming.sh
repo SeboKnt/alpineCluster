d201881f8f9034eaa03b0adcee6fa1f525956e56
Sebastian <105110061+JosebStahl@users.noreply.github.com>
2023-02-04 22:10:20 +0000

    add terraform

--- priming.sh
+++ priming.sh
@@ -6,6 +6,13 @@ sudo apt install ansible -y
 
 ssh-keygen -t rsa -b 4096
 
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+terraform init
+terraform apply
+
 server=("schwalbe" "spatz" "zwerggans")
 for hosts in "${server[@]}"
 do
