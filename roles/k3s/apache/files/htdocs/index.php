a015eb95884f1b1d266aa53e9524dbc7ea3aaa7f
Sebastian <105110061+JosebStahl@users.noreply.github.com>
2023-02-05 18:02:27 +0000

    only with kubectl cp

--- roles/k3s/apache/files/htdocs/index.php
+++ roles/k3s/apache/files/htdocs/index.php
@@ -1,8 +1,26 @@
-<html>
- <head>
-  <title>PHP-Test</title>
- </head>
- <body>
- <?php echo '<p>Hallo Welt</p>'; ?>
- </body>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script defer src="functions.js"></script>
+</head>
+
+<body class="body">
+    <div class="loginbox">
+    <img src="./bilder/login2.png" class="avatar">
+        <h1>Melden sie sich an!</h1>  
+        <form id="login-form">
+            <p>Kartennummer</p>
+            <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Kartennummer">
+            <p>PIN / Passwort</p>
+            <input type="password" name="password" id="password-field" class="login-form-field" placeholder="**************">
+            <input type="submit" value="Login" id="login-form-submit">
+        </form>  
+    </div>
+    
+</body>
 </html>
