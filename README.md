533f13974986d077e6ad9f7200e56f22d94f42bf
sebastian <sebastian.alxr@gmail.com>
2023-03-30 10:48:13 +0200

    update

---
+++ README.md
@@ -0,0 +1,24 @@
+# Alpine Cluster
+
+## Description
+
+With this project you can create a k3s Cluster from scratch. 
+
+- creates the server via terraform
+- installs k3s on the server
+- mounts flux repo
+
+## Usage
+
+First of all you will need a Hetzner Cloud Projekt and a Hetzner Cloud Token. Instructions can be found [here](https://docs.hetzner.com/cloud/api/getting-started/generating-api-token/)
+
+Then please check if Terraform and Ansible is installed on your device:
+
+``` 
+terraform-v
+ansible -v
+```
+
+If you are on github Codespaces you can use the bash script [prepCodespace.sh](prepCodespace.sh)
+
+After that please c
--- prepCodespace.sh
+++ prepCodespace.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+# check if we are in a Github codespace / Ubuntu environment
+if ! [  -n "$(uname -a | grep Ubuntu)" ]; then
+   echo "You are not on a ubuntu environment. This script prepares github codespace!"
+    exit
+fi
+
+# create ssh key; Your input is required :(
+ssh-keygen -t rsa -b 4096
+
+# install ansible
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt update -y
 sudo apt install ansible -y
 
-ssh-keygen -t rsa -b 4096
-
+# install terraform
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 
+# create the server
 terraform -chdir=./infrastructure/ init
 terraform -chdir=./infrastructure/ apply
 
 sleep 20s
 
+# preps the server for ansible
 server=("schwalbe" "spatz" "zwerggans" "colibri" "kiwi")
 for hosts in "${server[@]}"
 do
